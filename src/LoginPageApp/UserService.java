7c748d93b698004ac2bab3b15a846caf296b3cb1
Emre Sani <emresani1998@gmail.com>
2024-03-01 20:07:40 +0300

    validations added

--- src/LoginPageApp/UserService.java
+++ src/LoginPageApp/UserService.java
@@ -7,7 +7,7 @@ public class UserService {
 
     public Scanner scanner=new Scanner(System.in);
 
-    public Map<String,String> userInfos=new HashMap<>();//email=password////asd@gmail.com
+    public Map<String,String> userInfos=new HashMap<>();//K:email=V:password////asd@gmail.com
 
     //public List<User> users=new ArrayList<>();-->practice
 
@@ -56,19 +56,110 @@ public class UserService {
 
     //3-email ve şifre bilgisi olan kayıtlı üyenin giriş işlemini yapma
     public void login(){
+        System.out.println("Email giriniz: ");
+        String email=scanner.nextLine().trim();
 
+        //kullanıcı kaydı var mı?
+        if (this.userInfos.containsKey(email)){//TRUE ise şifre girilmeli
 
+            //şifreyi kontrol edelim, yanlış girildiğinde 3 kez tekrar imkanı verelim
+
+            boolean isWrong=true;
+            int counter=3;
+            while (isWrong && counter>0) {//F & T : F
+                System.out.println("Şifrenizi giriniz :");
+                String password=scanner.nextLine().trim();
+                //emaille şifre eşleşiyor mu?
+                if (this.userInfos.get(email).equals(password)){
+                    System.out.println("Harika, sisteme giriş yaptınız. Hoşgeldiniz:)");
+                    isWrong=false;
+                }else{
+                    counter--;//2,1,0
+                    if (counter==0){
+                        System.out.println("3 kez hatalı giriş yaptınız!");
+                    }else {
+                        System.out.println("Şifreniz yanlış, tekrar deneyiniz! Kalan hakkınız: "+counter);
+                    }
+                }
+            }
+        }else{
+            //böyle bir kulanıcı kaydı yok
+            System.out.println("Sisteme kayıtlı kullanıcı bulunamadı.");
+            System.out.println("Üyeyseniz bilgilerinizi kontrol ediniz, değilseniz üye olunuz!");
+
+        }
     }
 
 
-    //4-email doğrulama:ÖDEVVV
+    // 4-email doğrulama
     private boolean validateEmail(String email){
-        return true;
+        boolean isValid;
+        boolean hasSpace=email.contains(" ");
+        boolean isContainsAt=email.contains("@");//asd@asd
+
+
+        if (hasSpace){
+            System.out.println("Email boşluk içeremez!");
+            isValid=false;
+        }else if (!isContainsAt){
+            System.out.println("Email @ sembolünü içermelidir!");
+            isValid=false;
+        }else {
+            String firstPart=email.split("@")[0];
+            String secondPart=email.split("@")[1];
+
+            boolean isExistsInvalidCharacters=firstPart.replaceAll("[A-Za-z0-9-._]","").length()>0;//Ab123-?]-->?]
+            boolean checkEmailDomain=secondPart.equals("gmail.com")||
+                    secondPart.equals("hotmail.com")||
+                    secondPart.equals("yahoo.com");
+
+            if (isExistsInvalidCharacters){
+                System.out.println("Email kullanıcı adı harf, rakam ve -._ sembolleri dışında karakter içeremez!");
+            }else if (!checkEmailDomain){
+                System.out.println("Email gmail.com,hotmail.com,yahoo.com ile bitmelidir!");
+            }
+            isValid=!isExistsInvalidCharacters && checkEmailDomain;
+
+        }
+        if (!isValid){
+            System.out.println("Geçersiz email, tekrar deneyiniz!");
+        }
+        return isValid;
     }
 
-    //5-password doğrulama:ÖDEVV
+    //TODO: 5-password doğrulama:ÖDEVV
     private boolean validatePassword(String password){
-        return true;
+
+        boolean isValid;
+
+        boolean hasSpace=password.contains(" ");
+        boolean isLengthGtSix=password.length()>=6;
+        boolean upperLetter=password.replaceAll("[^A-Z]","").length()>0;//Abc1234-->A
+        boolean lowerLetter=password.replaceAll("[^a-z]","").length()>0;
+        boolean digits=password.replaceAll("[\\D]","").length()>0;
+        boolean symbols=password.replaceAll("[\\P{Punct}]","").length()>0;
+
+        if (hasSpace){
+            System.out.println("Şifre boşluk içeremez!");
+        }else if (!isLengthGtSix){
+            System.out.println("Şifre en az 6 karakter olmalıdır!");
+        } else if (!upperLetter) {
+            System.out.println("Şifre en az 1 tane büyük harf içermelidir!");
+        } else if (!lowerLetter) {
+            System.out.println("Şifre en az 1 tane küçük harf içermelidir!");
+        } else if (!digits) {
+            System.out.println("Şifre en az 1 tane rakam içermelidir!");
+        } else if (!symbols) {
+            System.out.println("Şifre en az 1 tane sembol içermelidir!");
+        }
+
+        isValid=!hasSpace && isLengthGtSix && upperLetter && lowerLetter && symbols && digits;
+
+        if (!isValid){
+            System.out.println("Geçersiz şifre , tekrar deneyiniz!");
+        }
+
+        return isValid;
     }
 
 
